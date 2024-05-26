b594751d7bec572bccfe87234b7a34cebaae70ec
moust1que <emiliensalpeteur@gmail.com>
2024-05-27 00:38:20 +0200

    Emilien
    - Game1 visu

---
+++ Assets/Code/Game1/UIFireAnim.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFireAnim : MonoBehaviour {
+	[SerializeField] private Image m_Image;
+
+	[SerializeField] private Sprite[] m_SpriteArray;
+	[SerializeField] private float m_Speed;
+
+	private int m_IndexSprite = 0;
+	private Coroutine m_AnimCoroutine;
+
+	private void Start() {
+		StartCoroutine(AnimCoroutine());
+	}
+
+	private IEnumerator AnimCoroutine() {
+		yield return new WaitForSeconds(m_Speed);
+		
+		if(m_IndexSprite >= m_SpriteArray.Length)
+			m_IndexSprite = 0;
+
+		m_Image.sprite = m_SpriteArray[m_IndexSprite];
+		m_IndexSprite++;
+
+		m_AnimCoroutine = StartCoroutine(AnimCoroutine());
+	}
+}
