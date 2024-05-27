48abcf0b2edc51e15e11a96d4d89611802473589
Maxime <117020630+Maxime-ai-jpg@users.noreply.github.com>
2024-05-27 18:35:58 +0200

    MAXIME
    spawn oryx ok
    oryx invisible dans le canvas
    et animation oryx casser

--- Assets/Code/Game 3/OryxMovement.cs
+++ Assets/Code/Game 3/OryxMovement.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 public class OryxMovement : MonoBehaviour
 {
+
     [SerializeField] private Vector3 vitesse;
   
     private void Start()
@@ -16,9 +17,9 @@ public class OryxMovement : MonoBehaviour
 
     void Update()
     {
-        transform.position = transform.position + new Vector3(UnityEngine.Random.Range(-500.0f, 300.0f), UnityEngine.Random.Range(-500.0f, 300.0f), 0) * Time.deltaTime;
+        // transform.position = transform.position + new Vector3(UnityEngine.Random.Range(-500.0f, 300.0f), UnityEngine.Random.Range(-500.0f, 300.0f), 0) * Time.deltaTime;
 
-        // transform.position = transform.position + vitesse * Time.deltaTime;
+        transform.position = transform.position + vitesse * Time.deltaTime;
 
     }
 }
